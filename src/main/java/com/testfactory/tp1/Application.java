c4ca0af1a1ca1786977a5422b3976a38c2ddf014
Willy DROUIN <w.drouin@iia-laval.fr>
2018-06-06 15:21:38 +0200

    first commit

---
+++ src/main/java/com/testfactory/tp1/Application.java
@@ -0,0 +1,13 @@
+package com.testfactory.tp1;
+
+public class Application {
+
+	public static void main (String[] args){
+		
+	}
+
+	public static int add(int i, int j) {
+		return i + j;
+	}
+	
+}
---
+++ src/test/java/com/testfactory/tp1/ApplicationTest.java
@@ -0,0 +1,18 @@
+package com.testfactory.tp1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class ApplicationTest {
+
+	@Test
+	public void testAdd() {
+		int i = 2, j = 3;
+		
+		int k = Application.add(i , j);
+			
+	    Assert.assertEquals("Add not good !", k , 5);
+	}
+
+}
