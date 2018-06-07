9d0f9949ec1f6222e23904323a6512e915150afd
Willy DROUIN <w.drouin@iia-laval.fr>
2018-06-07 14:44:09 +0200

    checkstyle

--- src/main/java/com/testfactory/tp1/Application.java
+++ src/main/java/com/testfactory/tp1/Application.java
@@ -1,8 +1,8 @@
 /****************************************************************************
- * Application java
- * 
- * Plein d'info 
- ***************************************************************************/
+ * Application java.
+ *
+ * Plein d'info.
+ * ***************************************************************************/
 package com.testfactory.tp1;
 
 /**
@@ -11,30 +11,26 @@ package com.testfactory.tp1;
  *
  */
 public final class Application {
-	
-	/**
-	 * Constructor
-	 */
-	private Application(){
-		
-	}
-	
-	/**
-	 * Main method
-	 * @param args table of string
-	 */
-	public static void main (final String[] args){
-		System.out.println("Hello baby");
-	}
 
-	/**
-	 * Add two numbers
-	 * @param number1 first integer
-	 * @param number2 second integer
-	 * @return an integer
-	 */
-	public static int add(final int number1,final int number2) {
-		return number1 + number2;
-	}
-	
+    /**
+    * Constructor.
+    */
+    private Application() { }
+    /**
+    * Main method.
+    * @param args table of string.
+    */
+    public static void main(final String[] args) {
+       System.out.println("Hello baby");
+    }
+
+     /**
+     * Add two numbers.
+     * @param number1 first integer.
+     * @param number2 second integer.
+     * @return an integer.
+    */
+    public static int add(final int number1, final int number2) {
+       return number1 + number2;
+    }
 }
