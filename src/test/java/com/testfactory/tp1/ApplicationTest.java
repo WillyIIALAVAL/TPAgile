efc6553027362920e3979fbac4332d34b8d56583
Willy DROUIN <w.drouin@iia-laval.fr>
2018-06-07 12:30:25 +0200

    pmd confirm

--- src/test/java/com/testfactory/tp1/ApplicationTest.java
+++ src/test/java/com/testfactory/tp1/ApplicationTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class ApplicationTest {
 	
 	@Test
-	public static void testAdd() {
+	public void testAdd() {
 		int i = 2, j = 3;
 		
 		int k = Application.add(i , j);
