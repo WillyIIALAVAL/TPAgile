daa979bc4b4c0709a7bc8cc6393b26c582665c0d
Willy DROUIN <w.drouin@iia-laval.fr>
2018-06-07 11:45:01 +0200

    commit

--- src/main/java/com/testfactory/tp1/Application.java
+++ src/main/java/com/testfactory/tp1/Application.java
@@ -1,11 +1,31 @@
+/****************************************************************************
+ * Application java
+ * 
+ * Plein d'info 
+ ***************************************************************************/
 package com.testfactory.tp1;
 
+/**
+ * My main application.
+ * @author Willy
+ *
+ */
 public class Application {
 
+	/**
+	 * Main method.
+	 * @param args. 
+	 */
 	public static void main (String[] args){
-		
+		System.out.println("Hello baby");
 	}
 
+	/**
+	 * Add two value.
+	 * @param i first value.
+	 * @param j second value.
+	 * @return result of addition.
+	 */
 	public static int add(int i, int j) {
 		return i + j;
 	}
